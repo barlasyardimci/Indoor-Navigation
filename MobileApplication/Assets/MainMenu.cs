d95ae79d8793c073fe4f8368b36a2343be793cc1
Ege Dinçer <egedincer1997@gmail.com>
2020-02-28 01:00:47 +0300

    Add files via upload

---
+++ MobileApplication/Assets/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public void SceneNavigate()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void SceneMaps()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
+    }
+    public void SceneNavigateToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1 );
+
+    }
+    public void SceneMapsToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+
+    }
+}
+
