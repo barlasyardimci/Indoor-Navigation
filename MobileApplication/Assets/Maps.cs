5fa5b94c509efe7fed9b79ddc0cbc5ae950204f2
ekinozkan <61553691+ekinozkan@users.noreply.github.com>
2020-04-15 23:43:49 +0300

    UI tweak
    change in some UI elements and unnecesary hierarchy in scenes Menu and Maps

---
+++ MobileApplication/Assets/Maps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Maps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region UI Buttons
+
+    public void ClickBack()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    #endregion
+}
